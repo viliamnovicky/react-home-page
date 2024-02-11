10b74fd213af721836c87dec3af722a41a44fc51
viliamnovicky <viliamnovicky@gmail.com>
2024-02-11 23:00:34 +0100

    diesel modal

---
+++ src/features/Diesel/DieselModal.jsx
@@ -0,0 +1,17 @@
+const StyledDieselModal = styled.div`
+width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;`
+
+function DieselModal({station}) {
+    return (
+        <StyledDieselModal>
+            
+        </StyledDieselModal>
+    )
+}
+
+export default DieselModal
